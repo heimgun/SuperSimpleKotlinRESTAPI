51842c945f89eb2371b93a3c1853d3c83f7a077f
Heimgun <heidi.guneriussen@live.no>
2021-03-03 17:47:16 +0100

    API created

---
+++ src/test/kotlin/com/example/KotlinRESTService/KotlinRestServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinRESTService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
